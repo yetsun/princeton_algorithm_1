e48e774aef3b79e59730113e01014143d1747710
yetsun <yetsun@gmail.com>
2013-09-02 15:33:55 -0700

    update after assignment 2nd submit

--- Precolation/src/PercolationStats.java
+++ Precolation/src/PercolationStats.java
@@ -1,89 +1,95 @@
 public class PercolationStats {
-	Percolation p;
-	int N;
-	int T;
-	double[] x;
-	double mean;
-	double stddev;
-	
-	// perform T independent computational experiments on an N-by-N grid
-	public PercolationStats(int N, int T) {
-		this.N = N;
-		this.T = T;
-		this.x = new double[T];
-	}
-	
-	//[1, N)
-	//[0, 1) * n [0, N)
-	//[1, N+1)
+    private Percolation p;
+    private int N;
+    private int T;
+    private double[] x;
+    private double mean;
+    private double stddev;
 
-	// sample mean of percolation threshold
-	public double mean() {
-		long all = 0;
-		for(int c = 0; c < T; c++){
-			p = new Percolation(N);
-			
-			int t = 0;
-			int nn = N*N;
-			while(!p.percolates() && t < nn){
-				int i,j;
-				do{
-					 int rand = StdRandom.uniform(0, nn);
-		             i = rand / N + 1;
-		             j = rand % N + 1;
-				} while(p.isOpen(i, j));
-				
-				t++;
-				p.open(i, j);
-			}
-			
-			 x[c] = ((double)t / (double) (N * N));
-			
-			all += t;
-		}
-		mean = (double)all / (double)(T * N * N);
-		return mean;
-	}
+    // perform T independent computational experiments on an N-by-N grid
+    public PercolationStats(int N, int T) {
+        
+        if (N < 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
 
-	// sample standard deviation of percolation threshold
-	public double stddev() {
-		if(mean == 0 ) mean();
-		double sum = 0;
-		for(double d : x){
-			sum += (d- mean) * (d-mean);
-		}
-		stddev =  Math.sqrt(sum / (T-1));
-		return stddev;
-	}
+        this.N = N;
+        this.T = T;
+        this.x = new double[T];
+    }
 
-	// returns lower bound of the 95% confidence interval
-	public double confidenceLo() {
-		return mean - (1.96 * stddev / Math.sqrt(T));
-	}
+    // [1, N)
+    // [0, 1) * n [0, N)
+    // [1, N+1)
 
-	// returns upper bound of the 95% confidence interval
-	public double confidenceHi() {
-		return mean + (1.96 * stddev / Math.sqrt(T));
-	}
+    // sample mean of percolation threshold
+    public double mean() {
+        long all = 0;
+        for (int c = 0; c < T; c++) {
+            p = new Percolation(N);
 
-	// test client, described below
-	public static void main(String[] args) {
-		
-		int n = StdIn.readInt();
-		int t = StdIn.readInt();
-		
-		if(n < 0 || t < 0){
-			throw new IllegalArgumentException();
-		}
-		
-		PercolationStats ps = new PercolationStats(n,t);
-		StdOut.print("mean                    = ");
-		StdOut.println(ps.mean());
-		StdOut.print("stddev                  = ");
-		StdOut.println(ps.stddev());
-		StdOut.print("95% confidence interval = ");
-		StdOut.print(ps.confidenceLo());
-		StdOut.print(", ");
-		StdOut.print(ps.confidenceHi());
-	}
+            int t = 0;
+            int nn = N * N;
+            while (!p.percolates() && t < nn) {
+                int i, j;
+                do {
+                    int rand = StdRandom.uniform(0, nn);
+                    i = rand / N + 1;
+                    j = rand % N + 1;
+                } while (p.isOpen(i, j));
+
+                t++;
+                p.open(i, j);
+            }
+
+            x[c] = ((double) t / (double) (N * N));
+
+            all += t;
+        }
+        mean = (double) all / (double) (T * N * N);
+        return mean;
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        if (mean == 0)
+            mean();
+        double sum = 0;
+        for (double d : x) {
+            sum += (d - mean) * (d - mean);
+        }
+        stddev = Math.sqrt(sum / (T - 1));
+        return stddev;
+    }
+
+    // returns lower bound of the 95% confidence interval
+    public double confidenceLo() {
+        return mean - (1.96 * stddev / Math.sqrt(T));
+    }
+
+    // returns upper bound of the 95% confidence interval
+    public double confidenceHi() {
+        return mean + (1.96 * stddev / Math.sqrt(T));
+    }
+
+    // test client, described below
+    public static void main(String[] args) {
+
+        int n = StdIn.readInt();
+        int t = StdIn.readInt();
+
+        if (n < 0 || t < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        PercolationStats ps = new PercolationStats(n, t);
+        StdOut.print("mean                    = ");
+        StdOut.println(ps.mean());
+        StdOut.print("stddev                  = ");
+        StdOut.println(ps.stddev());
+        StdOut.print("95% confidence interval = ");
+        StdOut.print(ps.confidenceLo());
+        StdOut.print(", ");
+        StdOut.print(ps.confidenceHi());
+    }
 }
--- Precolation/src/Test.java
+++ Precolation/src/Test.java
@@ -3,14 +3,52 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Percolation p = new Percolation(3);
+/*		Percolation p = new Percolation(3);
 		p.open(1,  1);		
 		System.out.println(p.percolates());
 		p.open(2, 1);
 		
 		System.out.println(p.percolates());
 		p.open(3, 1);
-		System.out.println(p.percolates());
+		System.out.println(p.percolates());*/
+		
+		testcase_outofbound(10, 0, 6);
+		testcase_outofbound(10, 12, 6);
+		testcase_outofbound(10, 11, 6);
+		testcase_outofbound(10, 6, 0);
+		testcase_outofbound(10, 6, 12);
+		testcase_outofbound(10, 6, 11);
+	}
+	
+	public static void testcase_outofbound(int n, int i, int j){
+		
+		try{
+			testcase_open(n, i, j);
+		}catch (IndexOutOfBoundsException  e){}
+		
+		try{
+			testcase_isopen(n, i, j);
+		}catch (IndexOutOfBoundsException  e){}
+		
+		try{
+			testcase_isfull(n, i, j);
+		}catch (IndexOutOfBoundsException  e){}
+		
+	}
+	
+	public static void testcase_open(int n, int i, int j){
+		Percolation p = new Percolation(n);
+		p.open(i, j);
+	}
+	
+	public static void testcase_isopen(int n, int i, int j){
+		Percolation p = new Percolation(n);
+		p.isOpen(i, j);
+	}
+	
+	public static void testcase_isfull(int n, int i, int j){
+		Percolation p = new Percolation(n);
+		p.isFull(i, j);
 	}
 
 }
